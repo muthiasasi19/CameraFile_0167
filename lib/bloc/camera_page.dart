9b6ffcee9a9b550a0b5d18c0f782eb1d30610d9c
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-05-15 18:32:37 +0700

    Buat fungsi flash icon, tombol bulat, dan tampilan awal

--- lib/bloc/camera_page.dart
+++ lib/bloc/camera_page.dart
@@ -19,4 +19,38 @@ class _CameraPageState extends State<CameraPage> {
       bloc.add(InitializeCamera());
     }
   }
+
+  IconData _flasIcon(FlashMode mode) {
+    return switch (mode) {
+      FlashMode.auto => Icons.flash_auto,
+      FlashMode.always => Icons.flash_on,
+      _ => Icons.flash_off,
+    };
+  }
+
+  Widget _circleButton(IconData icon, VoidCallback onTap) {
+    return ClipOval(
+      child: Material(
+        color: Colors.white24,
+        child: InkWell(
+          onTap: onTap,
+          child: SizedBox(
+            width: 50,
+            height: 50,
+            child: Icon(icon, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Dashboard"), actions: const []),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(children: []),
+      ),
+    );
+  }
 }
