6ef942012fc5a8792f0028d79105dd7b1216e6aa
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-05-15 20:47:38 +0700

    Tambah variabel untuk mengatur kamera, mode flash, dan fitur zoom di CameraPage

--- lib/camera_page.dart
+++ lib/camera_page.dart
@@ -1,1 +1,24 @@
+import 'dart:io';
 
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({super.key});
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  late List<CameraDescription> _cameras;
+  CameraController? _controller;
+  int _selectedCameraIdx = 0;
+  FlashMode _flashMode = FlashMode.off;
+  double _zoom = 1.0;
+  double _minZoom = 1.0;
+  double _maxZoom = 1.0;
+  bool _isZoomSupported = false;
+}
