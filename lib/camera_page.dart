4a518058a040a52a78eaf4af812adb1b79681143
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-05-15 20:49:05 +0700

    Tambah inisialisasi dan setup kamera di initState

--- lib/camera_page.dart
+++ lib/camera_page.dart
@@ -21,4 +21,17 @@ class _CameraPageState extends State<CameraPage> {
   double _minZoom = 1.0;
   double _maxZoom = 1.0;
   bool _isZoomSupported = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    _initializeCamera();
+  }
+
+  Future<void> _initializeCamera() async {
+    _cameras = await availableCameras();
+    await _setupCamera(_selectedCameraIdx);
+  }
 }
