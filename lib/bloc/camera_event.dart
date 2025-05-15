0add2c9d1890381a32f0926cf15add631b3c9b61
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-05-15 18:01:03 +0700

    Tambah event ambil gambar dengan callback hasil foto

--- lib/bloc/camera_event.dart
+++ lib/bloc/camera_event.dart
@@ -7,3 +7,8 @@ final class InitializeCamera extends CameraEvent {}
 final class SwitchCamera extends CameraEvent {}
 
 final class ToggleFlash extends CameraEvent {}
+
+final class TakePicture extends CameraEvent {
+  final void Function(File imageFile) onPictureTaken;
+  TakePicture(this.onPictureTaken);
+}
