ddcf5fadee85c43948911a18e3829ecdf00e6c68
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-05-15 18:08:49 +0700

    Tambah event hapus gambar, hapus snackbar, dan izin akses

--- lib/bloc/camera_event.dart
+++ lib/bloc/camera_event.dart
@@ -25,3 +25,9 @@ final class OpenCameraAndCapture extends CameraEvent {
   final BuildContext context;
   OpenCameraAndCapture(this.context);
 }
+
+final class DeleteImage extends CameraEvent {}
+
+final class ClearSnackbar extends CameraEvent {}
+
+final class RequestPermissions extends CameraEvent {}
