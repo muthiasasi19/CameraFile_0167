3a34534ffc355b5d3c191d8e00137c968fec4dfb
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-05-15 18:23:22 +0700

    Buat halaman awal CameraPage sebagai StatefulWidget

---
+++ lib/bloc/camera_page.dart
@@ -0,0 +1,11 @@
+import 'package:camera/camera.dart';
+import 'package:camerafile/bloc/camera_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({super.key});
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
