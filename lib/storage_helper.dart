c31294f3987d7eea06f1d543efdadb02536ee596
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-05-15 13:54:25 +0700

    Tambahkan logika simpan gambar ke direktori

--- lib/storage_helper.dart
+++ lib/storage_helper.dart
@@ -9,4 +9,12 @@ class StorageHelper {
     if (!await dir.exists()) await dir.create(recursive: true);
     return dir.path;
   }
+
+  static Future<File> saveImage(File file, String prefix) async {
+    final String dirPath = await _getFolderPath();
+    final String fileName =
+        '$prefix${DateTime.now().millisecondsSinceEpoch}${path.extension(file.path)}';
+    final String savedPath = path.join(dirPath, fileName);
+    return await file.copy(savedPath);
+  }
 }
