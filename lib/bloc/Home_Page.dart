e00fbabe8555a3129e53f30b08c3a5fbedda7bba
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-05-15 21:12:21 +0700

    Buat halaman HomePage dengan tombol Ambil Foto dan Pilih dari Galeri menggunakan Bloc

--- lib/bloc/Home_Page.dart
+++ lib/bloc/Home_Page.dart
@@ -1,1 +1,55 @@
+import 'package:camerafile/bloc/camera_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Beranda")),
+      body: SafeArea(
+        child: BlocConsumer<CameraBloc, CameraState>(
+          listener: (context, state) {
+            if (state is CameraReady && state.snackbarMessage != null) {
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text(state.snackbarMessage!)));
+              context.read<CameraBloc>().add(ClearSnackbar());
+            }
+          },
+          builder: (context, state) {
+            // final File? imageFile = state is CameraReady ? state.imageFile : null;
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton.icon(
+                        icon: const Icon(Icons.camera),
+                        label: const Text('Ambil Foto'),
+                        onPressed: () {
+                          final bloc = context.read()<CameraBloc>();
+                          if (bloc.state is! CameraReady) {
+                            bloc.add(InitializeCamera());
+                          }
+                          bloc.add(OpenCameraAndCapture(context));
+                        },
+                      ),
+                    ),
+                    ElevatedButton.icon(
+                      icon: const Icon(Icons.folder),
+                      label: const Text('Pilih dari Galeri'),
+                      onPressed:
+                          () => context.read<CameraBloc>().add(
+                            PickImageFromGallery(),
+                          ),
+                    ),
+                  ],
+                ),
+    );
+  }
+}
 
